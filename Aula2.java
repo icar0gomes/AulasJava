0eb7cf0912f1568ce643546aff00000cff2d1562
Ícaro Gomes <icarxgomes@gmail.com>
2024-09-04 20:03:18 +0000

    aula finalizada

--- Aula2.java
+++ Aula2.java
@@ -2,9 +2,9 @@ import java.util.Scanner;
 
 public class Aula2{
     public static void main(String[] args){
-        System.out.println("Acredito que sim");
+        System.out.println("Escreva cinco nomes");
         Scanner leitorTxt = new Scanner(System.in);
-        System.out.println("Digite seu nome: \n");
+        System.out.println("Digite os: \n");
         String nome = leitorTxt.nextLine();
         System.out.println(nome);
         leitorTxt.close();
---
+++ Aula3.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Aula3 {
+
+    public static void main(String[] args) {
+
+        Scanner leitorTxt = new Scanner(System.in);
+
+        System.out.println("Digite um nome \n");
+        String nome1 = leitorTxt.nextLine();
+        System.out.println("Seu nome é " + nome1);
+        
+
+        System.out.println("Digite um nome \n");
+        String nome2 = leitorTxt.nextLine();
+        System.out.println("Seu nome é " + nome2);
+        
+
+        System.out.println("Digite um nome \n");
+        String nome3 = leitorTxt.nextLine();
+        System.out.println("Seu nome é " + nome3);
+        
+
+        System.out.println("Digite um nome \n");
+        String nome4 = leitorTxt.nextLine();
+        System.out.println("Seu nome é " + nome4);
+        
+
+        System.out.println("Digite um nome \n");
+        String nome5 = leitorTxt.nextLine();
+        System.out.println("Seu nome é " + nome5);
+        
+
+        leitorTxt.close();
+    System.out.println();
+        
+
+    }
+}
